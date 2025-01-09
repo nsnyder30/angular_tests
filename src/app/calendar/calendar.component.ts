0cf08ecb4f4e12992432786411d3ece40dcc4133
Neil Snyder <neil.r.snyder@gmail.com>
2025-01-09 15:08:52 -0800

    Create dedicated method for setting dates
    Fix date transition logic when switching between weeks

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,113 +1,158 @@
 import { Component } from '@angular/core';
 import { trigger, transition, state, style, animate, query, group } from '@angular/animations';
 
-type CalendarView = 'Yearly' | 'Monthly' | 'Weekly' | 'Daily';
-
 @Component({
 	selector: 'app-calendar',
 	standalone: false,
 	templateUrl: './calendar.component.html',
 	styleUrl: './calendar.component.scss', 
-	animations: [
-		trigger('testTransition', [
-			state('s1', style({ backgroundColor: '#F00' })), 
-			state('s2', style({ backgroundColor: '#00F' })), 
-			transition('s1 => s2', animate('500ms ease-in')), 
-			transition('s2 => s1', animate('500ms ease-out'))
-		]), 
-		trigger('viewTransition', [
-	  		transition('Yearly2Monthly', [
-				query(':enter, :leave', style({ position: 'absolute', width: '100%', height: '100%' }), { optional: true }), 
-				group([
-					query(':leave', [
-						style({ transform: 'scale(1)', opacity: 1 }), 
-						animate('500ms ease-in', style({ transform: 'scale(2)', opacity: 0}))
-					], { optional: true}), 
-					query(':enter', [
-						style({ transform: 'scale(0.5)', opacity: 0 }), 
-						animate('500ms ease-out', style({ transform: 'scale(1)', opacity: 1}))
-					], {optional: true })
-				])
-			]), 
-			transition('Monthly2Yearly', [
-				query(':enter, :leave', style({ position: 'absolute', width: '100%', height: '100%' }), { optional: true }), 
-				group([
-					query(':leave', [
-						style({ transform: 'scale(1)', opacity: 1 }), 
-						animate('500ms ease-in', style({ transform: 'scale(0.5)', opacity: 0}))
-					], { optional: true }), 
-					query(':enter', [
-						style({ transform: 'scale(2)', opacity: 0 }), 
-						animate('500ms ease-out', style({ transform: 'scale(1)', opacity: 1 }))
-					], { optional: true })
-				])
-			])
-		])
-	]
+	animations:  []
 })
 
 export class CalendarComponent {
-	month_list = ['January','February','Match','April','May','June','July','August','September','October','November','December'] as const;
-	view: CalendarView = 'Yearly';
-	selectedDate: Date = new Date();
-	testVar = 's1';
+	view = 'Yearly';
+	selectedDate = new Date();
+	selectedMonth = -1;
+	weekStart = this.selectedDate.getTime() - this.selectedDate.getDay() * 24 * 3600 * 1000;
+	weekEnd = this.weekStart + 6 * 24 * 3600 * 1000;
+	calendarTitle = this.selectedDate.getFullYear().toString();
 
-	toggleTest() {
-		this.testVar = this.testVar == 's1' ? 's2' : 's1'
+	months = Array.from({ length: 12 }).map((v, i) => i);
+	month_list = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+	weeks = Array.from({ length: 5 }).map((v, i) => i);
+	days = Array.from({ length: 35 }).map((v, i) => i);
+	
+	getMonthName(month: number, short?: boolean) {
+		return short ? this.month_list[month].substr(0, 3) : this.month_list[month];
 	}
 
-	switchView(view: CalendarView): void { 
-console.log({msg: 'switchView called', view: view});
+	getDayText(day: number) {
+		let dt = this.idx_to_date(day);
+		return dt.getDate();
+	}
+
+	updateTitle() {
+		switch(this.view) {
+			case 'Yearly':
+				this.calendarTitle = (this.selectedDate.getFullYear()).toString();
+				break;
+			case 'Monthly':
+				this.calendarTitle = [this.getMonthName(this.selectedDate.getMonth()), this.selectedDate.getFullYear().toString()].join(' ');
+				break;
+			case 'Weekly':
+				let week_start = new Date(this.selectedDate);
+				let week_end = new Date(this.selectedDate);
+				week_start.setDate(week_start.getDate() - week_start.getDay());
+				week_end.setDate(week_end.getDate() + 6 - week_end.getDay());
+				this.calendarTitle = [
+							[(week_start.getMonth() + 1).toString(), week_start.getDate().toString()].join('/'), 
+							[(week_end.getMonth() + 1).toString(), week_end.getDate().toString()].join('/')
+				].join(' - ');
+				break;
+			case 'Daily':
+				this.calendarTitle = [(this.selectedDate.getMonth() + 1).toString(), this.selectedDate.getDate().toString()].join('/');
+				break;
+		}
+	}
+
+	toggleView(view: string) {
 		this.view = view;
+		this.updateTitle();
+	}
+
+	setDate(dt: Date) {
+		this.selectedDate = dt;
+		this.weekStart = dt.getTime() - dt.getDay() * 24 * 3600 * 1000;
+		this.weekEnd = this.weekStart + 6 * 24 * 3600 * 1000;
+	}
+
+	transition(dir: string) {
+		const coeff = dir == 'next' ? 1 : -1;
+		let dt = new Date(this.selectedDate);
+		switch(this.view) {
+			case 'Yearly':
+				dt.setFullYear(dt.getFullYear() + coeff);
+				break;
+			case 'Monthly':
+				dt.setDate(1);
+				dt.setMonth(dt.getMonth() + coeff);
+				break;
+			case 'Weekly':
+				dt.setDate(dt.getDate() + 7 * coeff);
+				break;
+			case 'Daily':
+				dt.setDate(dt.getDate() + coeff);
+				break;
+
+		}
+		this.setDate(dt);
+		this.updateTitle();
+	}
+
+	titleClick() {
+		switch(this.view) {
+			case 'Monthly': this.toggleView('Yearly'); break;
+			case 'Weekly': this.toggleView('Monthly');break;
+			case 'Daily': this.toggleView('Weekly');break;
+		}
+	}
+	
+	idx_to_date(idx: number) {
+		let dt = new Date(this.selectedDate);
+		dt.setDate(1);
+		dt.setDate(dt.getDate() + idx - dt.getDay());
+		return dt;
+	}
+
+	selectDay(day: number) {
+		if(this.view == 'Monthly' || this.view == 'Weekly' || this.view == 'Daily') {
+			this.setDate(this.idx_to_date(day));
+			const newView = this.view == 'Weekly' ? 'Daily' : 'Weekly';
+			this.toggleView(newView);
+		}
 	}
 
 	selectMonth(month: number) {
-		this.selectedDate.setMonth(month);
-		this.switchView('Monthly');
+		if(this.selectedMonth == month && this.view == 'Monthly') {
+			this.selectedMonth = -1;
+			this.toggleView('Yearly');
+		} else {
+			this.selectedMonth = month;
+			this.setDate(new Date(this.selectedDate.getFullYear(), this.selectedMonth, 1));
+			this.toggleView('Monthly');
+		}
 	}
-
-	navigate(direction: 'previous' | 'next'): void {
-console.log({msg: 'navigate called', direction: direction });
-		const date = new Date(this.selectedDate);
-		const sgn = direction === 'next' ? 1 : -1;
-		if (this.view === 'Yearly') {
-			date.setFullYear(date.getFullYear() + sgn);
-		} else if (this.view === 'Monthly') {
-			date.setMonth(date.getMonth() + sgn);
-		} else if (this.view === 'Weekly') {
-			date.setDate(date.getDate() + sgn * 7);
-		} else if (this.view === 'Daily') {
-			date.setDate(date.getDate() + sgn);
+	
+	selection(target: string, view: string, idx: number) {
+		if(view != this.view) {
+			return false;
 		}
 
-		this.selectedDate = date;
-	}
-
-	getMonthName(mnth: number) {
-		mnth = mnth < 0 || mnth > 11 ? 0 : mnth;
-		return this.month_list[mnth];
-	}
-
-	getWeeks() {
-		let curDate = new Date(this.selectedDate);
-		curDate.setDate(1);
-		curDate.setDate(curDate.getDate() - curDate.getDay());
-
-		let endDate = new Date(this.selectedDate);
-		endDate.setDate(1);
-		endDate.setMonth(endDate.getMonth() + 1);
-		endDate.setDate(endDate.getDate() - 1);
-		endDate.setDate(endDate.getDate() + 6 - endDate.getDay());
-
-		const numWeeks = Math.ceil((endDate.getTime() - curDate.getTime()) / (7 * 24 * 3600 * 1000));
-		const weeks = Array.from({ length: numWeeks }, () => Array(7).fill(null));
-		for(let week = 0; week < numWeeks; week++) {
-			for(let day = 0; day < 7; day++) {
-				weeks[week][day] = curDate.getDate();
-				curDate.setDate(curDate.getDate() + 1);
-			}
+		switch(target) {
+			case 'month':
+				return this.selectedMonth === idx;
+				break;
+			case 'day':
+				let dt = this.idx_to_date(idx).getTime();
+				return view == 'Weekly' ? dt >= this.weekStart && dt <= this.weekEnd : dt == this.selectedDate.getTime();
+				break;
 		}
 
-		return weeks;
+		return false;
+	}
+
+	hide(target: string, idx: number) {
+		switch(target) {
+			case 'month':
+				return !(this.view == 'Monthly' && this.selectedMonth === idx || this.view == 'Yearly');
+				break;
+			case 'day':
+				let dt = this.idx_to_date(idx).getTime();
+				return !(this.view == 'Monthly' || this.view == 'Weekly' && dt >= this.weekStart && dt <= this.weekEnd || this.view == 'Daily' && dt == this.selectedDate.getTime());
+				break;
+		}
+
+		return false;
 	}
 }
