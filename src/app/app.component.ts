db484f0aee942d7713bc7636826ddd5249e67387
Neil Snyder <neil.r.snyder@gmail.com>
2025-01-10 13:49:59 -0800

    Define default header content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LayoutService } from './services/layout.service';
+import { HeaderService } from './services/header.service';
 
 @Component({
 	selector: 'app-root',
@@ -8,18 +9,26 @@ import { LayoutService } from './services/layout.service';
 	styleUrl: './app.component.scss'
 })
 
-export class AppComponent {
-	itle = 'angular_app';
+export class AppComponent implements OnInit {
+	title = 'angular_app';
 	layoutClasses = {};
+	headerContent: string = 'Default Header';
 	
-	constructor(private layoutService: LayoutService) {
+	constructor(private layoutService: LayoutService, private headerService: HeaderService) {
+	}
+
+	ngOnInit() {
 		this.layoutService.layoutState$.subscribe((state) => {
 			this.layoutClasses = { 
 				hasRpanel: state.hasRpanel, 
 				hasLpanel: state.hasLpanel, 
 				hasHeader: state.hasHeader, 
 				hasFooter: state.hasFooter
-			  };
-		  });
-	  }
+			};
+		});
+
+		this.headerService.headerContent$.subscribe(content => {
+			this.headerContent = content;
+		});
+	}
 }
