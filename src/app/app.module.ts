f5864fc684d633c81098f8cad821a35e5b4dedaf
Neil Snyder <neil.r.snyder@gmail.com>
2025-01-17 11:59:15 -0800

    Remove references to tutorial component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes, RouterOutlet } from '@angular/router';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
-import { TutorialComponent } from './tutorial/tutorial.component';
 import { LoginComponent } from './login/login.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
 import { ProjectGridComponent } from './project-grid/project-grid.component';
@@ -15,7 +14,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 const routes: Routes = [
 	{path: '', component: LoginComponent},
 	{path: 'test', component: TestComponent}, 
-	{path: 'tutorial', component: TutorialComponent}, 
 	{path: 'create-account', component: CreateAccountComponent}, 
 	{path: 'home', component: ProjectGridComponent}, 
 	{path: 'calendar', component: CalendarComponent}
@@ -27,7 +25,6 @@ const routes: Routes = [
 		AppComponent, 
 		LoginComponent, 
 		TestComponent,
-		TutorialComponent,
 		ProjectGridComponent,
 		CalendarComponent,
 	], 
