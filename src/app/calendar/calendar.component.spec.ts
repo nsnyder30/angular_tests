7f54615add6455f58a24720e467266a89e70282b
Neil Snyder <neil.r.snyder@gmail.com>
2025-01-17 14:39:15 -0800

    Define tests for toggling between calendar views

--- src/app/calendar/calendar.component.spec.ts
+++ src/app/calendar/calendar.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CalendarComponent } from './calendar.component';
+import { LayoutService } from '../services/layout.service';
+import { HeaderService } from '../services/header.service';
+import { StyleUpdateService } from '../services/style-update.service';
 
 describe('CalendarComponent', () => {
   let component: CalendarComponent;
@@ -8,13 +10,35 @@ describe('CalendarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [CalendarComponent]
+      declarations: [CalendarComponent], 
+      providers: [
+	      LayoutService, 
+	      HeaderService, 
+	      StyleUpdateService
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(CalendarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  it('should toggle the view and update the calendar title', ()  => {
+	  spyOn(component, 'updateTitle').and.callThrough();
+	  expect(component.view).toBe('Monthly');
+
+	  component.toggleView('Weekly');
+	  expect(component.view).toBe('Weekly');
+	  expect(component.updateTitle).toHaveBeenCalled();
+
+	  component.toggleView('Yearly');
+	  expect(component.view).toBe('Yearly');
+
+	  component.toggleView('Daily');
+	  expect(component.view).toBe('Daily');
+
+	  expect(component.updateTitle).toHaveBeenCalledTimes(3);
   });
 
   it('should create', () => {
