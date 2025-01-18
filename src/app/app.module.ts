7877240f215d875d7f94dfa15ae9b835e761589d
Neil Snyder <neil.r.snyder@gmail.com>
2025-01-17 17:42:29 -0800

    Add modules needed to implement angular/material dialogs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterModule, Routes, RouterOutlet } from '@angular/router';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
@@ -9,7 +15,7 @@ import { CreateAccountComponent } from './create-account/create-account.componen
 import { ProjectGridComponent } from './project-grid/project-grid.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { DatePipe } from '@angular/common';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 const routes: Routes = [
 	{path: '', component: LoginComponent},
@@ -30,16 +36,22 @@ const routes: Routes = [
 	], 
 	imports: [
 		BrowserModule, 
+		BrowserAnimationsModule,
+		FormsModule, 
+		ReactiveFormsModule, 
+		MatDialogModule, 
+		MatFormFieldModule, 
+		MatInputModule, 
+		MatButtonModule, 
 		HttpClientModule, 
 		RouterOutlet, 
 		RouterModule.forRoot(routes), 
-		BrowserAnimationsModule
 	], 
 	exports: [
 		RouterModule,
 		RouterOutlet
 	], 
-	providers: [DatePipe], 
+	providers: [DatePipe, provideAnimationsAsync()], 
 	bootstrap: [AppComponent]
 })
 
